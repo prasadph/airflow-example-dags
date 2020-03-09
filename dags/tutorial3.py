fb4a769d2dc4247dc0ca079a46b92c81d4c3d7bc
Prasad Honavar <prasadhonavar@gmail.com>
2020-03-09 17:29:36 +0530

    Update and rename tutorial2.py to tutorial3.py

--- dags/tutorial3.py
+++ dags/tutorial3.py
@@ -30,7 +30,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 dag = DAG(
-    'tutorial2',
+    'tutorial3',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
